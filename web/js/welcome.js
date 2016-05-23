77036ec99969b2f83d8f7b089575843bc0d2c4fa
Ian Alonso Rojas <Iansillu@MacBook-Pro-de-Ian.local>
2016-05-23 17:49:28 +0200

    end v2.0

--- web/js/welcome.js
+++ web/js/welcome.js
@@ -20,3 +20,13 @@
  	})
  }
 
+ function ComprovaPass(){
+ 	var pass1 = $('#ianser_userbundle_userregister_password_first').value();
+ 	alert(pass1);
+ 	var pass2 = $('#ianser_userbundle_userregister_password_second').value();
+
+ 	if(pass2 == pass1){
+ 		$('#ianser_userbundle_userregister_submit').prop( "disabled" , false );
+ 	}
+ }
+
